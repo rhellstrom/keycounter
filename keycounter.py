3ac0fd3c4114b403ae9d4184c41a7c1b97891f48
Rasmus Hellström <hellstromrasmus@gmail.com>
2024-05-12 15:03:03 +0300

    Added argparse for specifying what eventfile to read from

--- keycounter.py
+++ keycounter.py
@@ -1,13 +1,15 @@
 import subprocess
 import struct
+import argparse
 
 class MultipleEventFilesError(Exception):
     pass
+
 # From the output of /proc/bus/input/devices we can determine what eventfile
 # is used for the keyboard. EV value should always be 120013
 # Mice can also show up as a keyboard so until we figure out another way to filter
 # output we return error if more than one file is found
-def get_keyboard_device_filename(): 
+def get_keyboard_device_filename() -> str: 
     command_str = "grep -E 'Handlers|EV' /proc/bus/input/devices | grep -B1 120013 | grep -Eo 'event[0-9]+'"
     res = subprocess.run(['sh', '-c', command_str], capture_output=True, text=True)
     event_file = res.stdout.strip()
@@ -16,14 +18,27 @@ def get_keyboard_device_filename():
         raise MultipleEventFilesError("Multiple event files found. Please specify event file as an argument.")
     return f"/dev/input/{event_file}" 
 
-def main():
-    try: 
-        device_file_path = get_keyboard_device_filename()
-    except MultipleEventFilesError as e:
-        print(e)
-        exit(1)
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--event-file", help="Keyboard event file. e.g /dev/input/event13",default=None)
+    args = parser.parse_args()
+    return args
 
-    device_file = open(device_file_path.strip(), "rb")
+def main() -> None:
+    args = parse_args()
+
+    device_file_path = ""
+    if args.event_file:
+       device_file_path = args.event_file 
+    else:
+        try: 
+            device_file_path = get_keyboard_device_filename()
+        except MultipleEventFilesError as e:
+            print(e)
+            exit(1)
+
+    device_file = open(device_file_path, "rb")
+
     counter = 0
     while True:
         # kernel.org/doc/Documentation/input/input.txt
@@ -33,8 +48,6 @@ def main():
     
         key_code = unpacked_data[5]
         key_value = unpacked_data[6]
-        # print(unpacked_data)
-        print("", key_code)
         if key_value == 1 and key_code in [26, 39, 40]:
             counter += 1
         print(counter)
